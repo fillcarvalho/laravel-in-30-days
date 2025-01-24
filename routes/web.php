c0b5d08e52f39ed3c57ba88c6b15ee6ae478ebd0
Filipe Costa <filipe@rhizatech.com.br>
2025-01-24 15:51:50 -0300

    Creating the first route and files

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', static function () {
+    return view('home');
+});
+
+Route::get('/about', static function () {
+    return view('about');
+});
+
+Route::get('/about2', static function () {
+    return ['foo' => 'bar'];
+});
+
+Route::get('/about3', static function () {
+    return "about page";
+});
+
+Route::get('/contact', static function () {
+    return view('contact');
 });
